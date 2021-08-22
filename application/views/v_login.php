ef19b3527454a393d0cfc878fae9752e561c1d42
iqballa24 <iqbalnugraha347@gmail.com>
2021-08-22 14:00:08 +0700

    update login

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -24,13 +24,10 @@
                 <div class="login-box mx-auto" >
                     <!-- /.login-logo -->
                     <div class="card">
-                        <div class="login-logo mt-4 mb-0">
+                        <div class="login-logo mt-4 mb-4">
                             <img src="<?= base_url("assets/img/logolsp.png"); ?>" alt="logo" width="120px">
                         </div>
                         <div class="card-body login-card-body">
-                            <h5 class="login-box-msg">
-                                <strong>Arsip</strong> LSP HCMI
-                            </h5> 
                             <?= form_error('username', '<div class="alert alert-warning" role="alert"><small class="text-white pl-3">', '</small></div>'); ?>
                             <form class="user" method="post" action="<?php echo site_url('admin/auth/login/'); ?>">
                                 <div class="input-group mb-3">
@@ -61,7 +58,7 @@
                         </div>
                         <!-- /.login-card-body -->
                         <hr>
-                        <p class="login-box-msg text-sm">Copyright © 2021 LSP HCMI. All rights reserved.</p>
+                        <p class="login-box-msg text-sm">Copyright © 2021 Arsip LSP HCMI. All rights reserved.</p>
                     </div>
                 </div>
             </div>
